eee08e654ffac846c9e495ee68a7653f714a48e5
Joshua Garcia <garcia.joshua2402@gmail.com>
2025-04-23 22:18:00 +0800

    icons paden

--- static/js/script.js
+++ static/js/script.js
@@ -23,16 +23,6 @@ const toggleSwitch = document.querySelector(
   '.theme-switch input[type="checkbox"]'
 );
 
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-  }
-}
-
-toggleSwitch.addEventListener("change", switchTheme, false);
-
 //  Store color theme for future visits
 
 function switchTheme(e) {
@@ -45,16 +35,21 @@ function switchTheme(e) {
   }
 }
 
+toggleSwitch.addEventListener("change", switchTheme, false);
+
 // Save user preference on load
 
 const currentTheme = localStorage.getItem("theme")
-  ? localStorage.getItem("theme")
-  : null;
-
-if (currentTheme) {
+ if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
-
   if (currentTheme === "dark") {
     toggleSwitch.checked = true;
   }
-}
+  else {
+    toggleSwitch.checked = false;
+  }
+ }
+ else {
+  document.documentElement.setAttribute("data-theme", "dark");
+  toggleSwitch.checked = true;
+ }
